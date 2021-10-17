3af907c0a4ef838457132385c2c844ebd3ecbcbe
chenyanyang <184536632@qq.com>
2021-10-17 22:55:47 +0800

    &&

---
+++ load.js
@@ -0,0 +1,239 @@
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+    // Getter
+    get area() {
+        return this.calcArea();
+    }
+    // Method
+    calcArea() {
+        return this.height * this.width;
+    }
+    }
+
+    const square = new Rectangle(10, 10);
+   // console.log(square.area);
+   //var myStatement = function() {
+    class Statement {
+        constructor(id, type, root, branch, reference, referee, desc, challenges)
+        {
+            this.id = id;
+            this.desc = desc;
+            this.type = type;
+            this.root = root;
+            this.branch = branch;
+            this.challenges = challenges
+            this.referee=referee;
+            this.reference=reference;
+        };
+
+        setId(id){
+            this.id = id;
+            return this;
+        };
+
+        setDesc(desc){
+            this.desc = desc;
+            return this;
+        };
+        setUrl(url){
+            this.url = url;
+            return this;
+        };
+        setType(type){
+            this.type = type;
+            return this;
+        };
+        setRoot(root){
+            this.root = root;
+            return this;
+        };
+        setBranch(branch){
+            this.branch = branch;
+            return this;
+        };
+        setReference(reference){
+            this.reference = reference;
+            return this;
+        };
+        setReferee(referee){
+            this.referee = referee;
+            return this;
+        };
+        setChallenges(challenges){
+            this.challenges = challenges;
+            return this;
+        }
+
+        
+    }
+//}
+
+    
+
+    // let StatementBuilder = function () {
+        
+    //     return {
+    //         setId: function (id){
+    //             this.id = id;
+    //             return this;
+    //         },
+    //         setDesc: function (desc){
+    //             this.desc = desc;
+    //             return this;
+    //         },
+    //         setType: function (type){
+    //             this.type = type;
+    //             return this;
+    //         },
+    //         setRoot: function (root){
+    //             this.root = root;
+    //             return this;
+    //         },
+    //         setBranch: function (branch){
+    //             this.branch = branch;
+    //             return this;
+    //         },
+    //         setReference: function (reference){
+    //             this.reference = reference;
+    //             return this;
+    //         },
+    //         setReferee: function (referee){
+    //             this.referee = referee;
+    //             return this;
+    //         },
+    //         build: function () {
+    //         return new Statement(this.id, this.type, this.root, this.branch, this.referee, this.reference, this.desc);
+    //     }
+    //     };
+    // };
+
+        //var expStmt = new Statement(["r1"],["b1"]);
+       // let bltStmt = new StatementBuilder().setType('b').setRoot('c').setBranch('d').setReference('referer').setReferee('').build();
+       // let bltStmt2 = new StatementBuilder().setType('b').setRoot('c').setBranch('d').setReference(bltStmt).setReferee('referee').build();
+        //console.log(expStmt);
+        //console.log(bltStmt2);
+
+        let godHelpNReign = new Statement().setId("godHelpNReign")
+.setDesc(
+    [   "Book of Deuteronomy chapter 6 : 6:21 6:21	你就告诉你的儿子说，我们在埃及作过法老的奴仆；耶和华用大能的手将我们从埃及领出来，",
+    "Then you will say to your son, We were Pharaoh's slaves in Egypt, and Jehovah brought us out of Egypt with a mighty hand.",
+    "6:22	在我们眼前，将极大、极重的神迹和奇事，施行在埃及，在法老并他全家的身上，",
+    "And Jehovah put forth before our eyes great and grievous signs and wonders in Egypt against Pharaoh and all his house.",
+    "6:23	将我们从那里领出来，要领我们进入祂向我们列祖起誓应许之地，把这地赐给我们。",
+    "Then He brought us out from there in order to bring us in, that He might give us the land which He swore to our fathers.",
+    "6:24	耶和华吩咐我们遵行这一切律例，敬畏耶和华我们的神，使我们常常得福，并蒙祂保全我们的生命，像今日一样。",
+    "And Jehovah commanded us to do all these statutes so that we would fear Jehovah our God for our good always and He would preserve us alive, as we are this day.",
+    "6:25	我们若在耶和华我们的神面前，照祂所吩咐我们的，谨守遵行这一切诫命，这就是我们的义了。",
+    "And it will be righteousness to us if we are certain to do all this commandment before Jehovah our God, as He commanded us.",
+        "Book of Judges Chapter 6: 6:8 耶和华就差遣一位申言者到以色列人那里，对他们说，耶和华以色列的神如此说，我曾领你们从埃及上来，带你们从为奴之家出来，",
+        "Jehovah sent a man who was a prophet to the children of Israel. And he said to them, Thus says Jehovah the God of Israel, It was I who brought you up out of Egypt and brought you out from the slave house;",
+        "6:9 救你们脱离埃及人的手，并脱离一切欺压你们之人的手，把他们从你们面前赶出，将他们的地赐给你们；",
+        "And I delivered you from the hand of the Egyptians and out of the hand of all who oppressed you. And I drove them out before you and gave you their land.",
+        "6:10	又对你们说，我是耶和华你们的神；你们住在亚摩利人的地，不可敬畏他们的神。你们竟不听从我的话。",
+        "And I said to you, I am Jehovah your God; you shall not fear the gods of the Amorites, in whose land you dwell. But you did not listen to My voice."
+    ]
+)
+
+
+let bloodLessons = new Statement()
+        .setId("bloodLessons")
+        .setDesc([
+"Book of Genesis Chapter 6:5 耶和华见人在地上罪恶甚大，心中终日所思念的尽都是恶",
+"And Jehovah saw that the wickedness of man was great in the earth, and that every imagination of the thoughts of his heart was only evil continually.",
+"6:13 神就对挪亚说，凡属肉体之人的结局，已经来到我面前；因为地上满了他们的强暴，我要把他们和地一并毁灭",
+"And God said to Noah, The end of all flesh has come before Me, for the earth is filled with violence because of them; and now I am about to destroy them with the earth."
+,"耶和华闻了那怡爽的香气，就心里说，我不再因人的缘故咒诅地，(人从小时心里怀着恶念，)也不再按着我才行的，灭各种的活物了。",
+"And Jehovah smelled the satisfying fragrance; \
+and Jehovah said in His heart, I will never again curse the ground on account of man, \
+for the imagination of man's heart is evil from his youth; nor will I ever again smite everything living as I have done.",
+"Book of Genesis Chapter 9:23	于是闪和雅弗拿件衣服搭在肩上，倒退着进去，遮盖他们父亲的赤身；他们背着脸，就没有看见父亲的赤身。",
+"And Shem and Japheth took a garment and laid it upon both their shoulders, and they walked backward and covered the nakedness of their father. And their faces were turned backward, so that they did not see their father's nakedness.",
+"9:24	挪亚醒了酒，知道小儿子向他所作的事，",
+"And Noah awoke from his wine and learned what his youngest son had done to him.",
+"9:25	就说，迦南当受咒诅，必给他弟兄作奴仆的奴仆。",
+"And he said, Cursed be Canaan: A servant of servants Shall he be to his brothers.",
+"Book of Isaiah 14: ",
+"14:12	明亮之星，清晨之子阿，你何竟从天坠落！你这攻败列国的，何竟被砍倒在地上！",
+"How you have fallen from heaven, O Daystar, son of the dawn! How you have been hewn down to earth, You who made nations fall prostrate!",
+"14:13	你心里曾说，我要升到天上；我要高举我的宝座在神众星以上。我要坐在聚会的山上，在北方的极处。",
+"But you, you said in your heart: I will ascend to heaven; Above the stars of God I will exalt my throne. And I will sit upon the mount of assembly In the uttermost parts of the north.",
+"14:14	我要升到高云之上；我要使自己与至高者一样。",
+"I will ascend above the heights of the clouds; I will make myself like the Most High.",
+"14:15	然而你必坠落阴间，到坑中极深之处。",
+"But you will be brought down to Sheol, To the uttermost parts of the pit."
+        ])
+
+let commandments = new Statement()
+.setId("commandments")
+.setDesc([
+    "Book of Genesis Chapter 9:3 凡活着的动物都可以作你们的食物；这一切我都赐给你们，如同我将菜蔬赐给你们一样。",
+    "Every moving thing that lives shall be food for you; just as I gave you the green herb, so I have given you everything.",
+    "9:4	惟独肉带着生命，就是带着血，你们不可吃。",
+    "But flesh with its life, that is, its blood, you shall not eat.",
+    "9:5	你们生命的血，我必追讨。向兽，我必追讨；向人，向各人的弟兄，我也必追讨人的命。",
+    "And indeed your lifeblood I will require; from every animal I will require it. And from every man, from every man's brother, I will require the life of man.",
+    "9:6	凡流人血的，他的血也必被人所流，因为神造人是按自己的形像造的。",
+    "Whoever sheds man's blood, By man shall his blood be shed, For in the image of God He made man.",
+    "9:11	我与你们立约，凡有血肉的，不再被洪水灭绝，也不再有洪水毁坏地了。",
+    "And I establish My covenant with you, that never again will all flesh be cut off by the waters of the flood, and never again will there be a flood to destroy the earth."
+])
+
+let heaven = new Statement().setId("heaven")
+.setDesc([
+"Book of Psalms 48:",
+"48:2	在北面的锡安山，是大君王的城，居高华美，为全地所喜悦。",
+"Beautiful in elevation, The joy of the whole earth, Is Mount Zion, the sides of the north, The city of the great King.",
+"Book of Psalms 75:",
+"75:3	地和其上一切的居民，都融化了；我曾立了地的柱子。　细拉",
+"The earth and all who dwell in it melt; I have established its pillars.",
+"75:4	我对狂傲人说，不要狂傲；对凶恶人说，不要举角；",
+"I said to the boastful, Do not boast; And to the wicked, Do not lift up the horn.",
+"75:5	不要把你们的角高举；不要挺着颈项说狂妄话。",
+"Do not lift your horn on high; Do not speak arrogantly with a haughty neck.",
+"75:6	因为高举非从东，非从西，也非从南而来。",
+"For neither from the east nor from the west, And neither from the south, does exaltation come;",
+"75:7	惟有神断定：祂使这人降卑，使那人升高。",
+"For God is the Judge: He puts this one down and exalts that one.",
+"75:8	耶和华手里有杯，其中的酒起沫；杯内满了搀杂之物，祂倒出来；地上的恶人必都喝这酒的渣滓，而且喝尽。",
+"For there is a cup in the hand of Jehovah, And the wine foams; it is full of mixture, And He pours from it; indeed its dregs will all the wicked of the earth Drain off and drink up.",
+"75:9	至于我，我要宣扬直到永远；我要歌颂雅各的神。",
+"But as for me, I will declare forever, I will sing psalms to the God of Jacob.",
+"Book of Isaiah",
+"4:13	你心里曾说，我要升到天上；我要高举我的宝座在神众星以上。我要坐在聚会的山上，在北方的极处。",
+"But you, you said in your heart: I will ascend to heaven; Above the stars of God I will exalt my throne. And I will sit upon the mount of assembly In the uttermost parts of the north."
+])
+
+let overcomer = new Statement();
+
+let righteousness = new Statement().setId("righteousness").setDesc([
+"Book of Deuteronomy chapter 6",
+"http://www.ephraim.site/b/len1/159.htm",
+"这是耶和华你们神吩咐我教训你们的诫命、律例和典章，使你们在所要过去得为业的地上遵行；",
+"Now this is the commandment, the statutes and the judgments, which Jehovah your God has commanded me to teach you, that you may do them in the land into which you are crossing over to possess;",
+"Book of Isaiah chapter 14",
+"14:29	非利士阿，你们众人不要因击打你们的杖折断就喜乐，因为从蛇的根必生出毒蛇；牠所生的是会飞的火蛇。",
+"Do not rejoice, all of you, Philistia, That the rod that strikes you is broken, For from the serpent's root a viper will come forth, And his fruit will be a flying fiery serpent.",
+"14:30	最贫寒的人必有所食，穷乏人必安然躺卧。我必以饥荒治死你的根，你所余剩的人必被杀戮。",
+"And the poorest of the poor will feed, And the needy will lie down securely. And I will kill your root by famine, And he will slay your remnant."
+]);
+
+let glorious = new Statement().setId("glorious")
+.setDesc([
+    "Book of Judges Chapter 5 : 5:1	那日，底波拉和亚比挪庵的儿子巴拉唱歌，说，",
+    "Then Deborah and Barak the son of Abinoam sang on that day, saying:",
+    "5:2	因为以色列中有首领统率，百姓也甘心奉献自己，你们应当颂赞耶和华。",
+    "That the leaders took the lead in Israel, That the people have willingly offered themselves, Bless Jehovah.",
+    "5:3	君王阿，要听！掌权者阿，要侧耳听！我要向耶和华歌唱；我要歌颂耶和华以色列的神。",
+    "Hear, O kings; give ear, O princes! I will sing; I, to Jehovah. I will sing praise to Jehovah The God of Israel."
+]);
+
+let greatPlan = new Statement().setId("greatPlan")
+.setDesc([
+    "5:15	以萨迦的首领与底波拉同来；以萨迦以忠诚待巴拉，众人都跟随巴拉冲下山谷。在流便的族系中，有心中定大志的。",
+    "And the princes in Issachar were with Deborah; And Issachar was true to Barak; Into the valley they were sent behind him. Among the divisions of Reuben There were great resolutions in heart.",
+    "5:16	你为何坐在羊圈之间，听召唤群羊的笛声呢？在流便的族系中，有心中设大谋的。",
+    "Why did you sit among the sheepfolds Listening to the pipings for the flocks? In the divisions of Reuben There were great searchings of heart."
+]);
